da74b11e1b6523e9999a2aad9132ffff46995c0c
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-03-09 21:59:11 +0800

    Grid RWD

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,12 @@
 export default function NavBar() {
     return (
         <nav className="header text-center d-flex row flex-column">
-            
+            <p className="text-white pt-5 pb-2">Tableware</p>
+            <p>Cookware</p>
+            <p>Home accessories</p>
+            <p>Lighting</p>
+            <p>Textile</p>
+            <p>Furniture</p>
         </nav>
     );
 
