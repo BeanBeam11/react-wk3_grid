870a9f3c858d6b352e14724720bc56f33f2d5a53
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-03-09 22:13:25 +0800

    NavBar.js -> navItem

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 export default function NavBar() {
     return (
-        <nav className="header text-center d-flex row flex-column">
-            <p className="text-white pt-5 pb-2">Tableware</p>
-            <p>Cookware</p>
-            <p>Home accessories</p>
-            <p>Lighting</p>
-            <p>Textile</p>
-            <p>Furniture</p>
-        </nav>
+        <ul className="navBar bg-color list-unstyled">
+            <li className="navItem">Tableware</li>
+            <li className="navItem">Cookware</li>
+            <li className="navItem">Home accessories</li>
+            <li className="navItem">Lighting</li>
+            <li className="navItem">Textile</li>
+            <li className="navItem">Furniture</li>
+        </ul>
     );
 
 }
